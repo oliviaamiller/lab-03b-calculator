ac59e64067173109d72708a3725b68184975d30e
oliviaamiller <oliviaoliviaolivia@gmail.com>
2021-11-05 16:18:13 -0700

    fixed typo, now all sum buttons are working

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ import { add, subtract, multiply, divide } from './utils.js';
 const firstInput = document.getElementById('first-num');
 const secondInput = document.getElementById('second-num');
 const addButton = document.getElementById('add-button');
+const subtractButton = document.getElementById('subtract-button');
+const multiplyButton = document.getElementById('multiply-button');
+const divideButton = document.getElementById('divide-button');
 const sumHere = document.getElementById('sum-here');
 
 
@@ -11,6 +14,25 @@ addButton.addEventListener('click', () => {
     const firstNum = +firstInput.value;
     const secondNum = +secondInput.value;
     const result = add(firstNum, secondNum);
-    sumHere.textContext = result;
+    sumHere.textContent = result;
+
+});
+
+subtractButton.addEventListener('click', () => {
+    const firstNum = +firstInput.value;
+    const secondNum = +secondInput.value;
+    sumHere.textContent = subtract(firstNum, secondNum);
+});
+
+multiplyButton.addEventListener('click', () => {
+    const firstNum = +firstInput.value;
+    const secondNum = +secondInput.value;
+    sumHere.textContent = multiply(firstNum, secondNum);
+});
+
+divideButton.addEventListener('click', () => {
+    const firstNum = +firstInput.value;
+    const secondNum = +secondInput.value;
+    sumHere.textContent = divide(firstNum, secondNum);
 
 });
